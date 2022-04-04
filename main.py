cf612f6203dddf68961b453223f1edbb1861bd5c
Brante <brandon.porte@ynov.com>
2022-04-04 16:33:46 +0200

    begin of correction, correction bug

--- main.py
+++ main.py
@@ -5,6 +5,7 @@ from pathlib import Path
 #show question and write answer player in a file
 def game(question, nameAnime, root_dir):
     window = t.Screen()
+    window.clear()
     window.title("quizz")
     window.bgcolor('gray')
     window.setup(width=1600, height = 1400)
@@ -17,11 +18,48 @@ def game(question, nameAnime, root_dir):
     pen.hideturtle()
     pen.goto(0,0)
     pen.write("{}".format(question), align = 'center', font = ('Arial', 32, 'normal'))
-    time.sleep(20)
+    time.sleep(3)
     answer_player = t.textinput("answer", "Your answer ?")
     source_file = root_dir / "answers_player.txt"
     with open(source_file, "a", encoding="utf-8") as answer_player_file:
         answer_player_file.write(answer_player + "\n")
+
+def correction(nameAnime, root_dir):
+    window = t.Screen()
+    window.clear()
+    window.title("quizz")
+    window.bgcolor('gray')
+    window.setup(width=1600, height = 1400)
+    window.tracer(0)
+
+    pen = t.Turtle()
+    pen.speed(0)
+    pen.color("White")
+    pen.penup()
+    pen.hideturtle()
+    pen.goto(0,0)
+    pen.write("correction".format(question), align = 'center', font = ('Arial', 32, 'normal'))
+    time.sleep(5)
+
+    his_source_file = root_dir / "answers_player.txt"
+    newNameAnime = "answers_" + nameAnime
+    answers_source_file = root_dir / newNameAnime
+
+    his_answer = []
+    correct_answers = []
+        
+    with open(his_source_file) as his_answer_file:
+        for line in his_answer_file:
+            his_answer.append(line)
+
+    with open(answers_source_file) as correct_answer_file:
+        for line in correct_answer_file:
+            correct_answers.append(line)
+    
+    print(his_answer)
+    print(correct_answers)
+    
+
 
 
 if __name__ == "__main__":
@@ -59,4 +97,6 @@ if __name__ == "__main__":
 
     for question in questions:
         game(question, nameAnime, root_dir)
+    
+    correction(nameAnime, root_dir)
     
